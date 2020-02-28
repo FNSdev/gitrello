b3f192114aedbae690436d29172bab57aa99a9d9
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-02-28 13:22:36 +0300

    Added basic password validation into CreateUserSerializer

--- gitrello/authentication/api/serializers.py
+++ gitrello/authentication/api/serializers.py
@@ -1,4 +1,8 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
+
+from authentication.models import User
 
 
 class CreateUserSerializer(serializers.Serializer):
@@ -7,3 +11,18 @@ class CreateUserSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=30)
     last_name = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=128)
+
+    def validate(self, attrs):
+        user = User(
+            username=attrs['username'],
+            email=attrs['email'],
+            first_name=attrs['first_name'],
+            last_name=attrs['last_name'],
+        )
+
+        try:
+            validate_password(password=attrs['password'], user=user)
+        except ValidationError as e:
+            raise serializers.ValidationError(detail=e.messages)
+
+        return attrs

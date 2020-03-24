e7db7a04eee0f490674dbfa6811688085cadd3e1
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-03-24 21:24:59 +0300

    Serve static files from GoogleCloudStorage

--- gitrello/gitrello/settings_prod.py
+++ gitrello/gitrello/settings_prod.py
@@ -1,5 +1,11 @@
 from gitrello.settings import *
 
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'gitrello.me',
+    'www.gitrello.me',
+]
+
 # Mailing
 
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
@@ -9,3 +15,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+# Static
+STATIC_URL = f'https://storage.googleapis.com/{os.getenv("GS_BUCKET_NAME")}/static/'

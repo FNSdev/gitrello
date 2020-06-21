0a18892f8a6542a659a31b89cfd5cdf4c8110eab
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-06-21 12:32:51 +0300

    Bump dependencies. Disable CI/CD

--- gitrello/gitrello/settings_test.py
+++ gitrello/gitrello/settings_test.py
@@ -2,13 +2,14 @@ from gitrello.settings import *
 
 SECRET_KEY = 'TEST_SECRET_KEY'
 
+# TODO host will be different in kubernetes cluster
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DJANGO_DB_NAME'),
-        'USER': os.getenv('DJANGO_DB_USER'),
-        'PASSWORD': os.getenv('DJANGO_DB_PASSWORD'),
-        'HOST': os.getenv('DJANGO_DB_HOST'),
-        'PORT': os.getenv('DJANGO_DB_PORT'),
+        'ENGINE': 'django_cockroachdb',
+        'NAME': 'gitrello',
+        'USER': 'gitrello',
+        'PASSWORD': None,
+        'HOST': '127.0.0.1',
+        'PORT': '26257',
     }
 }
